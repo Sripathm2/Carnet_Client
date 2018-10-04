20bc01b83931db9ccaf7506c59671cd1d9b3ab76
chands1997 <shivangi.chand1997@gmail.com>
2018-10-04 14:29:18 -0400

    abc

--- src/App.js
+++ src/App.js
@@ -5,16 +5,27 @@ import Create_user from './components/Create_user'
 import Home from './components/Home'
 import Dashboard from './components/Dashboard'
 import Route from "react-router/es/Route";
+import axios from "axios";
 
+let data = axios.get('https://carnet-api.herokuapp.com/', {
+})
+    .then(function (response) {
+        return response.data
+    })
+    .catch(function (error) {
+        console.log(error);
+    });
 const App = () => (
     <Router>
         <div>
-            <p>testing testing</p>
+            <p>testing testing {data}</p>
             <Route exact path="/" component={Home} />
             <Route path="/create_user" component={Create_user} />
             <Route path="/dashboard" component={Dashboard} />
         </div>
     </Router>
 );
+
+
 
 export default App;
