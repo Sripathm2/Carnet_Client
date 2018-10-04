b08bd5aadbf7b61de3836491f4c23a5933ea098b
chands1997 <shivangi.chand1997@gmail.com>
2018-10-04 13:48:02 -0400

    Create User

--- src/App.js
+++ src/App.js
@@ -9,20 +9,6 @@ import Route from "react-router/es/Route";
 const App = () => (
     <Router>
         <div>
-            <ul>
-                <li>
-                    <Link to="/">Home</Link>
-                </li>
-                <li>
-                    <Link to="/create_user">Create User</Link>
-                </li>
-                <li>
-                    <Link to="/dashboard">Dashboard</Link>
-                </li>
-            </ul>
-
-            <hr />
-
             <Route exact path="/" component={Home} />
             <Route path="/create_user" component={Create_user} />
             <Route path="/dashboard" component={Dashboard} />
--- src/components/Create_user.js
+++ src/components/Create_user.js
@@ -1,9 +1,50 @@
 import React from 'react';
+import '../css/signup-grid.css';
+import '../css/standardize.css';
+import '../css/styles.css';
+import axios from 'axios'
 
 const Create_user = () => (
-    <div className="App">
-        //todo add html file
+    <div className="App body page-signup clearfix">
+        <div className="container _element container-1"></div>
+        <div className="container _element container-2"></div>
+        <input className="_input _input-1" placeholder="First Name" type="text"/>
+        <input className="_input _input-2" placeholder="Last Name" type="text"/>
+        <label>
+            email:
+            <input className="_input _input-3" placeholder="Email Address" type="email"/>
+        </label>
+        <label>
+            username:
+            <input className="_input _input-4" placeholder="User Name" type="text"/>
+        </label>
+        <label>
+            pass:
+            <input className="_input _input-6" placeholder="Password" type="password"/>
+        </label>
+        <input className="_input _input-9" placeholder="Re-Enter Password" type="password"/>
+        <label>
+            securityQ:
+            <input className="_input _input-12" placeholder="Enter Security Question" type="text"/>
+        </label>
+        <p className="text text-2">Welcome!</p>
+        <label>
+            securityA:
+            <input className="_input _input-18" placeholder="Answer" type="text"/>
+        </label>
+        <button className="_button _button-4" onClick={signUp}>Sign Up</button>
+        <p className="text text-5">Already have a Account ? <a>Login</a></p>
     </div>
 );
 
+function signUp() {
+    axios.post('https://carnet-api.herokuapp.com/user/register', {
+    })
+        .then(function (response) {
+            console.log(response);
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+}
 export default Create_user;
