556e8fb5c23ffff3733e3da8c0bd1b608ad3dc4c
chands1997 <shivangi.chand1997@gmail.com>
2018-10-04 15:19:56 -0400

    Completely Integrated Sign up

--- src/App.js
+++ src/App.js
@@ -5,20 +5,11 @@ import Create_user from './components/Create_user'
 import Home from './components/Home'
 import Dashboard from './components/Dashboard'
 import Route from "react-router/es/Route";
-import axios from "axios";
 
-let data = axios.get('https://carnet-api.herokuapp.com/', {
-})
-    .then(function (response) {
-        return response.data
-    })
-    .catch(function (error) {
-        console.log(error);
-    });
 const App = () => (
     <Router>
         <div>
-            <p>testing testing {data}</p>
+            <p>testing testing </p>
             <Route exact path="/" component={Home} />
             <Route path="/create_user" component={Create_user} />
             <Route path="/dashboard" component={Dashboard} />
--- src/components/Create_user.js
+++ src/components/Create_user.js
@@ -8,37 +8,43 @@ const Create_user = () => (
     <div className="App body page-signup clearfix">
         <div className="container _element container-1"></div>
         <div className="container _element container-2"></div>
-        <input className="_input _input-1" placeholder="First Name" type="text"/>
-        <input className="_input _input-2" placeholder="Last Name" type="text"/>
-        <label>
-            email:
-            <input className="_input _input-3" placeholder="Email Address" type="email"/>
-        </label>
-        <label>
-            username:
-            <input className="_input _input-4" placeholder="User Name" type="text"/>
-        </label>
-        <label>
-            pass:
-            <input className="_input _input-6" placeholder="Password" type="password"/>
-        </label>
-        <input className="_input _input-9" placeholder="Re-Enter Password" type="password"/>
-        <label>
-            securityQ:
-            <input className="_input _input-12" placeholder="Enter Security Question" type="text"/>
-        </label>
+        <input id ="firstName" className="_input _input-1" placeholder="First Name" type="text"/>
+        <input id = "lastName" className="_input _input-2" placeholder="Last Name" type="text"/>
+        <input id = "emailId" className="_input _input-3" placeholder="Email Address" type="email"/>
+        <input id = "userName" className="_input _input-4" placeholder="User Name" type="text"/>
+        <input id = "password" className="_input _input-6" placeholder="Password" type="password"/>
+        <input id = "rePass" className="_input _input-9" placeholder="Re-Enter Password" type="password"/>
+        <input id = "SQ" className="_input _input-12" placeholder="Enter Security Question" type="text"/>
         <p className="text text-2">Welcome!</p>
-        <label>
-            securityA:
-            <input className="_input _input-18" placeholder="Answer" type="text"/>
-        </label>
+        <input id = "SA" className="_input _input-18" placeholder="Answer" type="text"/>
         <button className="_button _button-4" onClick={signUp}>Sign Up</button>
         <p className="text text-5">Already have a Account ? <a>Login</a></p>
     </div>
 );
 
 function signUp() {
-    axios.post('https://carnet-api.herokuapp.com/user/register', { headers: {'Access-Control-Allow-Origin': 'https://carnet-client.herokuapp.com'} }, {
+    let userdata = {};
+    userdata.userName = document.getElementById("userName").value;
+    userdata.password = document.getElementById("password").value;
+    userdata.email = document.getElementById("emailId").value;
+    userdata.securityQuestion = document.getElementById("SQ").value;
+    userdata.securityAnswer = document.getElementById("SA").value;
+    userdata.fname = document.getElementById("firstName").value;
+    userdata.lname = document.getElementById("lastName").value;
+    //todo: passowrd check 
+
+    console.log(userdata.userName);
+    axios({
+        method:'post',
+        url:'https://carnet-api.herokuapp.com/user/register',
+        data:{
+            userName:userdata.userName,
+            password:userdata.password,
+            email: userdata.email,
+            securityQuestion: userdata.securityQuestion,
+            securityAnswer: userdata.securityAnswer,
+            name: userdata.fname +" "+ userdata.lname,
+        }
     })
         .then(function (response) {
             console.log(response);
