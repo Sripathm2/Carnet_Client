9d69845242ef7244a65645365c7ff59b8c59e2fb
Sripathm22 <mishra60@purdue.edu>
2018-10-30 12:09:02 -0400

    dashboard ui complete.

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,30 +1,30 @@
 import React from 'react';
-import '../css/dashboard.css'
+import '../css/DashboardStyle.css';
+import '../css/dashboard-grid.css';
+import logo from '../res/logo.jpg';
+import user from '../res/user.jpg'
 const Dashboard = () => (
     <div className="App">
-        <div id="container">
-            <div id="header">
-                <div id="brand">
-                    <h1><a>Carnet Inc.</a></h1>
-                </div>
-                <div id="searchbox">
-                    <form method="get">
-                        <input type="text" className="text"/>
-                            <input type="submit" value="Search" className="submit "/>
-                    </form>
-                </div>
+        <div className="containerhead clearfix">
+            <div className="containerlogo">
+                <img src={logo} alt="logo" style={{width: 100, height: 84}}/>
             </div>
-
-            <footer className="footer">
-                <div>
-                    <p>Carnet Inc. Copyright &copy; 2018. Pooja Tewari, Shivangi Chand, Siddharth Dhar, Sripath Mishra,
-                       </p>
-                </div>
-            </footer>
-
-
+            <input className="_input" type="text"/>
+            <button className="_button">Search</button>
         </div>
-
+        <div className="containerside clearfix">
+            <div className="containeruserimage">
+                <img src={user} alt="user" style={{width: 100, height: 100}}/>
+            </div>
+            <p className="textname"><span>Name of the User</span></p>
+            <p className="textsubs">Subscribers: NaN</p>
+            <p className="textemail">Email Address of the User&nbsp;</p>
+        </div>
+        <div className="containermain clearfix">
+            <button className="_button _button-2">Notebook</button>
+            <button className="_button _button-5">Notifications</button>
+            <div className="containernotebook"></div>
+        </div>
     </div>
 );
 
